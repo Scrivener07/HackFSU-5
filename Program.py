9ad4c6d1c4dc6e63778ebf2989be0689a246975f
Scrivener07 <ScrivenerMods@gmail.com>
2018-03-03 10:08:00 -0500

    use self

--- Program.py
+++ Program.py
@@ -11,59 +11,59 @@ class Database():
 		self.Dictionary[self.defaultProfile.UID] = self.defaultProfile
 	
 	def Add(self, element):
-		database.Dictionary[element.UID] = element
+		self.Dictionary[element.UID] = element
 
 	def Serialize(self):
 		with open(Database.FILENAME, 'w', newline='') as csvfile:
 			writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=Profile.Fields)
 			writer.writeheader()
 
-			for key, value in Database.Dictionary.items():
+			for key, value in self.Dictionary.items():
 				print(str(key))
 				print(str(value))
-
 				writer.writerow(
 					{
 						'Name': value.Name,
 						'Password': value.Password,
 						'Location': value.Location,
-						'HomeColor': value.HomeColor,
-						'CarColor': value.CarColor,
-						'HotelColor': value.HotelColor
+						'HomeValue': value.HomeValue,
+						'CarValue': value.CarValue,
+						'HotelValue': value.HotelValue
 					})
 
 	def Deserialize(self):
 		with open(Database.FILENAME, newline='') as csvfile:
 			reader = csv.DictReader(csvfile)
 			for row in reader:
-				print(row['Name'], row['Password'], row['Location'], row['HomeColor'], row['CarColor'], row['HotelColor'])
+				print(row['Name'], row['Password'], row['Location'], row['HomeValue'], row['CarValue'], row['HotelValue'])
 
 class Profile():
 	NextUID = count(-1)
-	Fields = ['Name', 'Password', 'Location', 'HomeColor', 'CarColor', 'HotelColor']
+	Fields = ['Name', 'Password', 'Location', 'HomeValue', 'CarValue', 'HotelValue']
 	def __init__(self, name, password):
 		self.UID = next(self.NextUID)
 		self.Name = name
 		self.Password = password
-		self.Location = Location.Nill
-		self.HomeColor = Color.Nill
-		self.CarColor = Color.Nill
-		self.HotelColor = Color.Nill
+		self.Location = Location.Unknown
+		self.HomeValue = Preference.Unknown
+		self.CarValue = Preference.Unknown
+		self.HotelValue = Preference.Unknown
 
 	def __str__(self):
 		return self.Name + " UID:" + str(self.UID)
 
-class Color(Enum):
-	Nill = 0
-	Red = 1
-	Yellow = 2
-	Blue = 3
+class Preference(Enum):
+	Unknown = -1
+	Off = 0
+	On = 1
+	Slow = 2
+	Fast = 3
 
 class Location(Enum):
-	Nill = 0
-	Home = 1
-	Car = 2
-	Hotel = 3
+	Unknown = -1
+	Home = 0
+	Car = 1
+	Hotel = 2
 
 #----------------------------------------------------------
 #----------------------------------------------------------
@@ -71,24 +71,24 @@ class Location(Enum):
 database = Database()
 
 kyleProfile = Profile("Kyle", 4401)
-kyleProfile.HomeColor = Color.Yellow
-kyleProfile.CarColor = Color.Yellow
-kyleProfile.HotelColor = Color.Yellow
+kyleProfile.HomeValue = Preference.On
+kyleProfile.CarValue = Preference.Fast
+kyleProfile.HotelValue = Preference.Slow
 
 jonahProfile = Profile("Jonah", 2018)
-jonahProfile.HomeColor = Color.Red
-jonahProfile.CarColor = Color.Red
-jonahProfile.HotelColor = Color.Red
+jonahProfile.HomeValue = Preference.Fast
+jonahProfile.CarValue = Preference.Slow
+jonahProfile.HotelValue = Preference.Off
 
 mattProfile = Profile("Matt", 1234)
-mattProfile.HomeColor = Color.Blue
-mattProfile.CarColor = Color.Blue
-mattProfile.HotelColor = Color.Blue
+mattProfile.HomeValue = Preference.Slow
+mattProfile.CarValue = Preference.Off
+mattProfile.HotelValue = Preference.Fast
 
 nathanProfile = Profile("Nathan", 8975)
-nathanProfile.HomeColor = Color.Yellow
-nathanProfile.CarColor = Color.Red
-nathanProfile.HotelColor = Color.Blue
+nathanProfile.HomeValue = Preference.Slow
+nathanProfile.CarValue = Preference.On
+nathanProfile.HotelValue = Preference.Slow
 
 database.Add(kyleProfile)
 database.Add(jonahProfile)
